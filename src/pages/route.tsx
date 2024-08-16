f2c65d89155a9cab7d40ab9b17ef994f64222e2e
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-16 16:11:38 +0330

    init(route): route page component created

---
+++ src/pages/route.tsx
@@ -0,0 +1,11 @@
+import Layout from '../layout/layout';
+import Start from './Start page/Start';
+const Route = () => {
+    return (
+        <Layout>
+            <Start />
+        </Layout>
+    );
+}
+
+export default Route;
