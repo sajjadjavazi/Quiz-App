2476917c4527472c4a83b5eac65ce6f293e885b7
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 22:09:58 +0330

    fix(apiUrls): number of questions fixed in urls

--- src/utils/apiUrls.ts
+++ src/utils/apiUrls.ts
@@ -1,16 +1,16 @@
-export const getApiUrl = (category: string, difficulty: string): string => {
+export const getApiUrl = (category: string, difficulty: string, numberOfQuestions: number): string => {
     const baseApiUrl = 'https://opentdb.com/api.php?';
-    
+
     const apiUrls: Record<string, string> = {
-        'general-easy': `${baseApiUrl}amount=50&category=9&difficulty=easy&type=multiple`,
-        'general-medium': `${baseApiUrl}amount=50&category=9&difficulty=medium&type=multiple`,
-        'general-hard': `${baseApiUrl}amount=50&category=9&difficulty=hard&type=multiple`,
-        'science-easy': `${baseApiUrl}amount=50&category=18&difficulty=easy&type=multiple`,
-        'science-medium': `${baseApiUrl}amount=50&category=18&difficulty=medium&type=multiple`,
-        'science-hard': `${baseApiUrl}amount=50&category=18&difficulty=hard&type=multiple`,
-        'history-easy': `${baseApiUrl}amount=50&category=23&difficulty=easy&type=multiple`,
-        'history-medium': `${baseApiUrl}amount=10&category=23&difficulty=medium&type=multiple`,
-        'history-hard': `${baseApiUrl}amount=10&category=23&difficulty=hard&type=multiple`,
+        'general-easy': `${baseApiUrl}amount=${numberOfQuestions}&category=9&difficulty=easy&type=multiple`,
+        'general-medium': `${baseApiUrl}amount=${numberOfQuestions}&category=9&difficulty=medium&type=multiple`,
+        'general-hard': `${baseApiUrl}amount=${numberOfQuestions}&category=9&difficulty=hard&type=multiple`,
+        'science-easy': `${baseApiUrl}amount=${numberOfQuestions}&category=18&difficulty=easy&type=multiple`,
+        'science-medium': `${baseApiUrl}amount=${numberOfQuestions}&category=18&difficulty=medium&type=multiple`,
+        'science-hard': `${baseApiUrl}amount=${numberOfQuestions}&category=18&difficulty=hard&type=multiple`,
+        'history-easy': `${baseApiUrl}amount=${numberOfQuestions}&category=23&difficulty=easy&type=multiple`,
+        'history-medium': `${baseApiUrl}amount=${numberOfQuestions}&category=23&difficulty=medium&type=multiple`,
+        'history-hard': `${baseApiUrl}amount=${numberOfQuestions}&category=23&difficulty=hard&type=multiple`,
     };
 
     return apiUrls[`${category}-${difficulty}`];
