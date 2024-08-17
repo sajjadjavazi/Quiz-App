9b212e2a0e1c1410824e2ed35eb28ba688eb9a87
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 16:02:09 +0330

    feat(setup): setup UI created

---
+++ src/pages/Setup page/SetupPage.tsx
@@ -0,0 +1,34 @@
+import { Box, Card, CardContent, FormControl, InputLabel, MenuItem, Select, Typography, Button } from "@mui/material";
+
+const SetupPage = () => {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
+            <Typography variant="h4" gutterBottom>Setup Your Quiz</Typography>
+            <Card sx={{ width: '90%', maxWidth: '500px', marginBottom: '20px' }}>
+                <CardContent>
+                    <FormControl fullWidth sx={{ marginBottom: '20px' }}>
+                        <InputLabel>Category</InputLabel>
+                        <Select defaultValue="">
+                            <MenuItem value="general">General Knowledge</MenuItem>
+                            <MenuItem value="science">Science</MenuItem>
+                            <MenuItem value="history">History</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl fullWidth sx={{ marginBottom: '20px' }}>
+                        <InputLabel>Difficulty</InputLabel>
+                        <Select defaultValue="">
+                            <MenuItem value="easy">Easy</MenuItem>
+                            <MenuItem value="medium">Medium</MenuItem>
+                            <MenuItem value="hard">Hard</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button variant="contained" color="primary" fullWidth>
+                        Start Quiz
+                    </Button>
+                </CardContent>
+            </Card>
+        </Box>
+    );
+};
+
+export default SetupPage;
