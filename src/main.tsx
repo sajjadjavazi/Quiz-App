32ccde3b8113e5cb5125e30e5c696befc7af8f4a
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 20:19:20 +0330

    feat(main): quiz provider imported

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { QuizProvider } from './context/QuizContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </StrictMode>,
 )
