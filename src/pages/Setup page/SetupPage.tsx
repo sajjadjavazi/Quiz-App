d1f14728c5ef9f7f9da266d748e7ea7b38d1af9a
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 16:28:16 +0330

    feat(setup): inputs fixed and slider created

--- src/pages/Setup page/SetupPage.tsx
+++ src/pages/Setup page/SetupPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, FormControl, InputLabel, MenuItem, Select, Typography, Button } from "@mui/material";
+import { Box, Card, CardContent, FormControl, InputLabel, MenuItem, Select, Typography, Button, Slider } from "@mui/material";
 
 const SetupPage = () => {
     return (
@@ -6,9 +6,9 @@ const SetupPage = () => {
             <Typography variant="h4" gutterBottom>Setup Your Quiz</Typography>
             <Card sx={{ width: '90%', maxWidth: '500px', marginBottom: '20px' }}>
                 <CardContent>
-                    <FormControl fullWidth sx={{ marginBottom: '20px' }}>
+                    <FormControl fullWidth sx={{ marginBottom: '20px', color:"primary" }}>
                         <InputLabel>Category</InputLabel>
-                        <Select defaultValue="">
+                        <Select defaultValue="" label="Category">
                             <MenuItem value="general">General Knowledge</MenuItem>
                             <MenuItem value="science">Science</MenuItem>
                             <MenuItem value="history">History</MenuItem>
@@ -16,12 +16,14 @@ const SetupPage = () => {
                     </FormControl>
                     <FormControl fullWidth sx={{ marginBottom: '20px' }}>
                         <InputLabel>Difficulty</InputLabel>
-                        <Select defaultValue="">
+                        <Select defaultValue="" label="Difficulty">
                             <MenuItem value="easy">Easy</MenuItem>
                             <MenuItem value="medium">Medium</MenuItem>
                             <MenuItem value="hard">Hard</MenuItem>
                         </Select>
                     </FormControl>
+                    <InputLabel>number of questions:</InputLabel>
+                    <Slider defaultValue={10} min={5} max={50} aria-label="Default" valueLabelDisplay="auto" sx={{ marginBottom: '20px' }}/>
                     <Button variant="contained" color="primary" fullWidth>
                         Start Quiz
                     </Button>
