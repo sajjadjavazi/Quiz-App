6a8f0fa5362e0ec049113e4423fd3b152a2cb143
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 22:26:54 +0330

    style(footer): code Prettier!

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -3,21 +3,21 @@ import { Box, Typography } from "@mui/material";
 const Footer = () => {
     return (
         <div>
-             <Box sx={{
-            position: 'fixed',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            backgroundColor: '#000000',
-            color: 'white',
-            textAlign: 'center',
-            padding: '10px 0',
-            zIndex: 1000,
-        }}>
-            <Typography variant="h5">
-                made with <span style={{ color: 'red' }}>♥</span> by Sajjad
-            </Typography>
-        </Box>
+            <Box sx={{
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                width: '100%',
+                backgroundColor: '#000000',
+                color: 'white',
+                textAlign: 'center',
+                padding: '10px 0',
+                zIndex: 1000,
+            }}>
+                <Typography variant="h5">
+                    made with <span style={{ color: 'red' }}>♥</span> by Sajjad
+                </Typography>
+            </Box>
 
         </div>
     );
