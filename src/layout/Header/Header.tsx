c4f39d8f60c3865dc952eb58c73d9b5c78ffc998
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 16:05:42 +0330

    feat(header): nav bar created

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,8 +1,21 @@
+import { Box, Typography } from "@mui/material"
+
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <Box sx={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            backgroundColor: 'purple',
+            color: 'white',
+            textAlign: 'center',
+            padding: '10px 0',
+            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+            zIndex: 1000,
+        }}>
+            <Typography variant="h4">Quiz App</Typography>
+        </Box>
     );
 }
 
