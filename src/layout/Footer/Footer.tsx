fd01225670571c2a2b6d011604455e9cc722c384
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-17 16:07:37 +0330

    feat(footer): footer with its typography created

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+import { Box, Typography } from "@mui/material";
+
 const Footer = () => {
     return (
         <div>
-            Footer
+             <Box sx={{
+            position: 'fixed',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            backgroundColor: '#000000',
+            color: 'white',
+            textAlign: 'center',
+            padding: '10px 0',
+            zIndex: 1000,
+        }}>
+            <Typography variant="h5">
+                made with <span style={{ color: 'red' }}>♥</span> by Sajjad
+            </Typography>
+        </Box>
+
         </div>
     );
 }
